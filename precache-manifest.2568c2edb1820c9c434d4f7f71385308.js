1c8145fd85574c251d6419d68c9cfca3a4799f53
Ryan Goderis <rgoderis789@gmail.com>
2019-11-01 10:01:19 -0400

    Updates

---
+++ precache-manifest.2568c2edb1820c9c434d4f7f71385308.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba8586aa69207d4ccf5a0daa384e63f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0c2af01d9b81d4e709ee",
+    "url": "/portfolio/static/css/main.9f573c20.chunk.css"
+  },
+  {
+    "revision": "37a41a54bbedeff30890",
+    "url": "/portfolio/static/js/2.96698dd5.chunk.js"
+  },
+  {
+    "revision": "0c2af01d9b81d4e709ee",
+    "url": "/portfolio/static/js/main.c16414a7.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "d1946edb3e2987fa2e32fb8ab4640891",
+    "url": "/portfolio/static/media/ClickyGame.d1946edb.png"
+  },
+  {
+    "revision": "27676d5794e39fe942f24885732ee706",
+    "url": "/portfolio/static/media/EatDaBurger.27676d57.png"
+  },
+  {
+    "revision": "6bea927f59185dace85f69fd4d53be09",
+    "url": "/portfolio/static/media/EventZip.6bea927f.png"
+  },
+  {
+    "revision": "d944a228e331114b6aa8efd4538bd5b2",
+    "url": "/portfolio/static/media/FriendFinder.d944a228.png"
+  },
+  {
+    "revision": "74a5fa0c1b33bfa7583bc8d790018832",
+    "url": "/portfolio/static/media/GifTastic.74a5fa0c.png"
+  },
+  {
+    "revision": "e00956e50bf2a524cf93379038d54af3",
+    "url": "/portfolio/static/media/Hangman.e00956e5.png"
+  },
+  {
+    "revision": "d0e770295ebcf0b1c6a3f5c912b06cff",
+    "url": "/portfolio/static/media/Headshot.d0e77029.jpg"
+  },
+  {
+    "revision": "b36cba9dcd6dd60cb017662165464441",
+    "url": "/portfolio/static/media/NYTScraper.b36cba9d.png"
+  },
+  {
+    "revision": "5b48560ba01794e4bff78a680a3a62ee",
+    "url": "/portfolio/static/media/PetSpressYourself.5b48560b.png"
+  },
+  {
+    "revision": "3e0b526ba47cd24668fd22e4b65b6976",
+    "url": "/portfolio/static/media/Psychic.3e0b526b.png"
+  },
+  {
+    "revision": "d2a758152b2a052696652d640bcb1d4f",
+    "url": "/portfolio/static/media/StarWarsRPG.d2a75815.png"
+  },
+  {
+    "revision": "c2453c2ed121a469aed6da29ac3b8941",
+    "url": "/portfolio/static/media/TrainScheduler.c2453c2e.png"
+  },
+  {
+    "revision": "e580b236a89f33180341bb3239ebf513",
+    "url": "/portfolio/static/media/Trivia.e580b236.png"
+  }
+]);
